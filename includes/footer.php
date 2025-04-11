310712f0e751b283e7d5e9b1b32f689814c0cb27
Sujal Kadam <162724689+Sujalkadam17@users.noreply.github.com>
2025-04-11 20:36:10 +0530

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,1 +1,8 @@
 
+<footer>
+    <p>🍛 Made with passion, teamwork, and love 💌</p>
+    <p>Copyright © <?= date("Y") ?> Recipe Finder Project</p>
+    <p>Built by Sujal, Sairaj & Rohit — who know more about debugging than actual cooking.</p>
+    <a href="https://www.instagram.com/cadet_sujal_kadam/">Instagram</a> | 
+    <a href="https://www.linkedin.com/in/sujal-kadam-2851a5261/">LinkedIn</a>
+</footer>
